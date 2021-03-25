966caebd16c8d2953c4b65084e2efdec971b4d9a
Brian Hayes <briancampbellhayes@icloud.com>
2021-03-24 23:01:14 -0500

    Gave the Employee index a button to comfirm pickups (nonfunctional)

--- TrashCollectorSite/Controllers/EmployeeController.cs
+++ TrashCollectorSite/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using TrashCollectorSite.Data;
 using TrashCollectorSite.Models;
@@ -21,13 +22,19 @@ namespace TrashCollectorSite.Controllers
         {
             //query customer table to find customers in same zip code as employee who is logged in and have a pickup that is today
 
-            //1. query employee table for logged in employee
-            //2. get today's day of week -- DateTime
-            //3. query the customer table for any customers that have the same zip as the logged in employee
-            //4. query the customers with same zip and find who has a pickup day that is equal to today
-            //5. pass those customers to the view
+            //1. ^^ query employee table for logged in employee 
+            //2. ^^ get today's day of week -- DateTime
+            //3. ^^ query the customer table for any customers that have the same zip as the logged in employee
+            //4. ^^ query the customers with same zip and find who has a pickup day that is equal to today
+            //5. ^^ pass those customers to the view
 
-            return View();
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var employee = _context.Customer.Where(c => c.IdentityUserId == userId).SingleOrDefault();
+            string day = DateTime.Now.DayOfWeek.ToString();
+            var zipCustomers = _context.Customer.Where(cust => cust.Zip.Equals(employee.Zip));
+            var todayPickup = zipCustomers.Where(cust => cust.PickUpDay == day);
+
+            return View(todayPickup);
         }
 
         // GET: EmployeeController1/Details/5
@@ -49,6 +56,8 @@ namespace TrashCollectorSite.Controllers
         {
             try
             {
+                var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                newEmployee.IdentityUserId = userId;
                 _context.Employee.Add(newEmployee);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
